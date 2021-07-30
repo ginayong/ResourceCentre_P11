483eda4440de0b2a29f7924416de6754d7bcab83
20045225 <20045225@DESKTOP-NJGC59J>
2021-07-30 11:23:26 +0800

    Updated: viewAllCamcorder(toString)

--- ResourceCentre_P11/src/Camcorder.java
+++ ResourceCentre_P11/src/Camcorder.java
@@ -1,5 +1,5 @@
 
-public class Camcorder extends Item{
+public class Camcorder extends Item {
 	private int opticalZoom;
 
 	public Camcorder(String assetTag, String description, int opticalZoom) {
@@ -10,11 +10,11 @@ public class Camcorder extends Item{
 	public int getOpticalZoom() {
 		return opticalZoom;
 	}
-	
-	public String toString(){
-		String output = "";
+
+	public String toString() {
 		// Write your codes here
+		String output = super.toString();
+		output = String.format("%-63s %-20d", output, opticalZoom);
 		return output;
 	}
 }
-
